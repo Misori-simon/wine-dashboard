1eb9dc85750ce790b6b26bc00eec6a59cb60ff6a
Misori Simon <misorisimon@gmail.com>
2023-01-06 07:42:49 +0100

    ->Styled the bottom row into tabs: Added tab buttons, Change flex direction of bottom row section, add 02(currentTab and showTabs) JS functions for the tab function

--- js/main.js
+++ js/main.js
@@ -37,3 +37,32 @@ let slideIndex = 1;
     slides[slideIndex-1].style.display = "block";
     discs[slideIndex-1].className += " active-disc";
     }
+
+
+    // for the carousel
+
+let tabIndex = 1;
+showtabs(tabIndex);
+
+// Thumbnail image controls
+function currentTab(n) {
+showtabs(tabIndex = n);
+}
+
+function showtabs(n) {
+let i;
+let tabs = document.getElementsByClassName("tab-item");
+console.log(tabs);
+let tabMenus = document.getElementsByClassName("tab");
+console.log(tabMenus);
+if (n > tabs.length) {tabIndex = 1}
+if (n < 1) {tabIndex = tabs.length}
+for (i = 0; i < tabs.length; i++) {
+    tabs[i].style.display = "none";
+}
+for (i = 0; i < tabMenus.length; i++) {
+    tabMenus[i].className = tabMenus[i].className.replace(" active-tab", "");
+}
+tabs[tabIndex-1].style.display = "block";
+tabMenus[tabIndex-1].className += " active-tab";
+}
