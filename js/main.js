fb972ef49f5d71ec1dd6bb802045ba808f19a2bd
Misori Simon <misorisimon@gmail.com>
2023-01-06 06:01:26 +0100

    ->Style the layout of top nav from flexbox with row items to a carousel with button controls, -> Added 03 functions for the slider, plusSlide, for next and prev controls; currentSlide, for the button controls below alider; and showSlides, to display slides

--- js/main.js
+++ js/main.js
@@ -1,8 +1,39 @@
 function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
+
+// for the carousel
+
+let slideIndex = 1;
+    showSlides(slideIndex);
+
+    // Next/previous controls
+    function plusSlides(n) {
+    showSlides(slideIndex += n);
     }
 
-    /* Set the width of the side navigation to 0 */
-    function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
+    // Thumbnail image controls
+    function currentSlide(n) {
+    showSlides(slideIndex = n);
+    }
+
+    function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("slide-item");
+    let discs = document.getElementsByClassName("disc");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < discs.length; i++) {
+        discs[i].className = discs[i].className.replace(" active-disc", "");
+    }
+    slides[slideIndex-1].style.display = "block";
+    discs[slideIndex-1].className += " active-disc";
     }
